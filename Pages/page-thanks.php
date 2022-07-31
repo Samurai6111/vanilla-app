3d8bb371dc2b8c993576534df56d3a782f3b579b
Samurai6111 <samurai.blue6111@gmail.com>
2022-07-31 17:58:17 +0900

    refactoring

--- Pages/page-thanks.php
+++ Pages/page-thanks.php
@@ -9,7 +9,6 @@ get_header(); ?>
 
 <main class="pageThanks -load-fade-in" id="pageThanks">
 	<br><br><br><br><br><br><br><br><br>
-	<br><br><br><br><br><br><br><br><br>
 
 	<div class="inner">
 		<h2 class="pageThanks__title">Thank you</h2>
