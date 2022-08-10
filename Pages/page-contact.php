3fe5bc3284660cd840b0561274c1ad3789a42e77
Samurai6111 <samurai.blue6111@gmail.com>
2022-08-10 11:06:42 +0900

    feat : ローディングアニメーション追加

--- Pages/page-contact.php
+++ Pages/page-contact.php
@@ -12,13 +12,23 @@ get_header(); ?>
 <main class="pageContact" id="pageContact">
 	<div class="inner">
 
-	<br><br><br><br><br>
-	<br><br><br><br><br>
+		<br><br><br><br><br>
+		<br><br><br><br><br>
 
 		<?php require_once(get_theme_file_path() . "/App/Form/Templates/contact-form-template.php") ?>
 	</div>
 
 
+	<script src="<?php echo get_template_directory_uri(); ?>/Assets/Js/Custom/loading-spinner-animation.js"></script>
+
+	<?php if (!isset($_GET['favorite-post'])) {
+	?>
+		<script>
+			$(function() {
+				loading_spinner_animation()
+			});
+		</script>
+	<?php } ?>
 
 </main>
 
