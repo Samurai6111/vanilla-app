da1a7e5c785e7740d46f8e33cc7a70689009bbb4
Samurai6111 <samurai.blue6111@gmail.com>
2022-08-24 17:37:40 +0900

    feat : configurationにスタイル追加

--- Pages/page-front.php
+++ Pages/page-front.php
@@ -10,49 +10,6 @@ get_header(); ?>
 <main class="pageFront" id="pageFront">
 	<div class="inner">
 		<div class="inner">
-			<ul class="">
-				<?php
-				$args = array(
-					'post_type' => 'test',
-					'posts_per_page' => -1,
-				);
-				$array = [];
-				$postslist = get_posts($args);
-				if ($postslist) {
-					foreach ($postslist as $post) {
-						setup_postdata($post);
-						$array_child = [];
-
-						$fields = get_field_objects();
-
-						if ($fields) {
-							foreach ($fields as $key => $values) {
-								$array_child[$key] = get_field($key);
-							}
-
-							$array[] = $array_child;
-						}
-
-					}
-				}
-				wp_reset_postdata();
-				$json = json_encode($array);
-
-
-				echo '<pre>';
-				var_dump('zxdfkajsd;lfj');
-				var_dump('zxdfkajsd;lfj');
-				var_dump('zxdfkajsd;lfj');
-				var_dump('zxdfkajsd;lfj');
-				var_dump('zxdfkajsd;lfj');
-				var_dump('zxdfkajsd;lfj');
-				var_dump('zxdfkajsd;lfj');
-				var_dump('zxdfkajsd;lfj');
-				var_dump('zxdfkajsd;lfj');
-				echo '</pre>';
-				echo $json;
-				 ?>
-			</ul>
 
 		</div>
 	</div>
