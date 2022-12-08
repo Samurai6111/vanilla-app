117da7d06d61beb7ae049d7d0c6a0ecdb54cea66
Samurai6111 <samurai.blue6111@gmail.com>
2022-12-09 06:04:44 +0900

    feat : 不要なコード削除

--- Feature-suumo/test.php
+++ Feature-suumo/test.php
@@ -1,15 +1,1 @@
-<?php
-$array = [
-
-  'rent' => 140000,
-  'initial_fee' => 579000,
-  'management_fee' => 5000,
-  'deposit' => 140000,
-  'retainer_fee' => 0,
-  'room_arragement' =>  '
-											3LDK',
-  'construction' =>  '-',
-  'address' =>  '
-									東京都豊島区南大塚３',
-  'link' =>  'https://suumo.jp/chintai/jnc_000078462314/kankyo/?bc=100301748806'
-];
+[object Object]
